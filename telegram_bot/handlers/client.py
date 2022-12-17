71cd0fe96bbfe2ac6143110fdc6a90efd4c30e8d
s1ch3r <116456446+s1ch3r@users.noreply.github.com>
2022-12-17 23:31:02 +0300

    Update client.py

--- telegram_bot/handlers/client.py
+++ telegram_bot/handlers/client.py
@@ -9,10 +9,11 @@ async def command_start(message : types.Message):
     await bot.send_message(message.from_user.id, 'Привет, я бот ПоисковичкОФФ. Я могу помочь тебе узнать что угодно. Вот список моих возможностей: \n1) /start и /help - показывают список основных комманд \n2) /Запрос - позволяет задать боту вопрос на который он подскажет ответ', reply_markup=kboard)
 
 #@dp.message_handler(commands=['Вопрос'])
+'''
 async def request_command(message : types.Message):
     await bot.send_message(message.from_user.id, 'Скоро ты сможешь получить здесь ответ на любой свой вопрос.') #, reply_markup=ReplyKeyboardRemove()                         #    await bot.send_message(message.from_user.id, 'Скоро ты сможешь получить сдесь ответ на любой свой вопрос.')
     await message.reply('Вот что нашлось в википедии по вашему запросу:')
-
+'''
 def register_handlers_client(dp : Dispatcher):
     dp.register_message_handler(command_start, commands=['start', 'help'])
-    dp.register_message_handler(request_command, commands=['Вопрос', 'вопрос'])
+    #dp.register_message_handler(request_command, commands=['Вопрос', 'вопрос'])
