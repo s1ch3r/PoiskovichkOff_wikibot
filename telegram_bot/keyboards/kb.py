e62ca1d5d211603b4f1e6c269979f602b6d54ef5
s1ch3r <116456446+s1ch3r@users.noreply.github.com>
2022-12-17 23:32:29 +0300

    Update kb.py

--- telegram_bot/keyboards/kb.py
+++ telegram_bot/keyboards/kb.py
@@ -3,7 +3,13 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton#, ReplyKeyboardRem
 
 b1 = KeyboardButton('/start')
 b2 = KeyboardButton('/Запрос')
+b3 = KeyboardButton('/Отмена')
+b4 = KeyboardButton('/Дополнительная_информация')
 
 kboard = ReplyKeyboardMarkup(resize_keyboard=True) #, one_time_keyboard=True
+cancel_kboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+additional_kboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 
 kboard.add(b1).add(b2)#.insert(b2)  .row(b1, b2)
+cancel_kboard.add(b3)
+additional_kboard.row(b4, b3)
