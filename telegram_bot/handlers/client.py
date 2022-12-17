65ec9090e03344df075d4a3b3f32def6eacdee59
s1ch3r <116456446+s1ch3r@users.noreply.github.com>
2022-12-17 23:29:55 +0300

    Update client.py

--- telegram_bot/handlers/client.py
+++ telegram_bot/handlers/client.py
@@ -12,6 +12,7 @@ async def command_start(message : types.Message):
 async def request_command(message : types.Message):
     await bot.send_message(message.from_user.id, 'Скоро ты сможешь получить здесь ответ на любой свой вопрос.') #, reply_markup=ReplyKeyboardRemove()                         #    await bot.send_message(message.from_user.id, 'Скоро ты сможешь получить сдесь ответ на любой свой вопрос.')
     await message.reply('Вот что нашлось в википедии по вашему запросу:')
+
 def register_handlers_client(dp : Dispatcher):
     dp.register_message_handler(command_start, commands=['start', 'help'])
     dp.register_message_handler(request_command, commands=['Вопрос', 'вопрос'])
