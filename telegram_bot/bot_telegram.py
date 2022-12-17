2da10d5e016bd2a71e112b59a3252fa6e46ac6f9
s1ch3r <116456446+s1ch3r@users.noreply.github.com>
2022-12-17 23:33:12 +0300

    Update bot_telegram.py

--- telegram_bot/bot_telegram.py
+++ telegram_bot/bot_telegram.py
@@ -16,5 +16,4 @@ other.register_handlers_other(dp)
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
 
 #переименовать client в usual_commands(usual), other в non_command_messages(non_commands), admin в wiki_request_command(wikipedia_request)
-#сделать кнопку отмены в машине состояний
-#доп вопросы в машине состояний новой функцией
+#инлайн клавиатура в доп вопросе??? мб тогда доп вопрос вне машины состяний, главное запоминать последний запрос пользователя
