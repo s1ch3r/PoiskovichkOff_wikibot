0462a0909872214e471baec12ae9e70f2db7148c
s1ch3r <116456446+s1ch3r@users.noreply.github.com>
2022-12-18 23:34:22 +0300

    Update random.py

--- telegram_bot/handlers/random.py
+++ telegram_bot/handlers/random.py
@@ -1,11 +1,9 @@
 from aiogram import types, Dispatcher
-#from create_bot import dp, bot
 from aiogram.dispatcher.filters import Text
 
 import wikipedia
 from bs4 import BeautifulSoup
 import requests
-#import webbrowser
 
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
@@ -23,19 +21,13 @@ class FSMRandom(StatesGroup):
 async def random_command(message : types.Message, state: FSMContext):
     await FSMRandom.answer.set()
     await message.answer('Бим бим, бам бам, бом бом. Здесь скоро будет рандом!', reply_markup=ReplyKeyboardRemove())
-        #question_wiki = "https://ru.wikipedia.org/wiki/Special:Random"
-        #await message.answer('Ссылка на статью: \n' + question_wiki.url)
-        #await message.answer('Название статьи: \n' + question_wiki.original_title)
-        #await message.answer('Статья: \n' + question_wiki.summary)
     url = requests.get("https://ru.wikipedia.org/wiki/Special:Random")
     soup = BeautifulSoup(url.content, "html.parser")
-    title = soup.find(class_="firstHeading").text #text
+    title = soup.find(class_="firstHeading").text
     async with state.proxy() as data:
-        data['answer'] = title #str(f"{title}")
-    await FSMRandom.next()  # перевод на следующее состояние
-    await message.answer(f"{title} \nВы хотите прочитать данную статью?", reply_markup=yesno_kboard)  ###################################### новую клаву
-      #.replace(" ", "_")
-        #question_wiki = wikipedia.page(str(data['question']))
+        data['answer'] = title
+    await FSMRandom.next() 
+    await message.answer(f"{title} \nВы хотите прочитать данную статью?", reply_markup=yesno_kboard)
 
 
 #@dp.message_handler(commands=['Да', 'Нет']) ###возможно нет надо отдельно
@@ -52,9 +44,6 @@ async def confirmation_command(message : types.Message, state: FSMContext):
     else:
         await message.answer('Запрос отменен. Чтобы продолжить работу введите /start или /help')
         await state.finish()
-
-#ans = message.text
-#if ans == "/Да" or answer == "Да" or answer == "да":
 
 
 
