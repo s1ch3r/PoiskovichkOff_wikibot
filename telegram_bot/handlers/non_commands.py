dcbb7f4a5b39c1de7f5789f984fb34d8ddbddbd0
s1ch3r <116456446+s1ch3r@users.noreply.github.com>
2022-12-18 23:33:00 +0300

    Update non_commands.py

--- telegram_bot/handlers/non_commands.py
+++ telegram_bot/handlers/non_commands.py
@@ -4,7 +4,6 @@ from create_bot import dp, bot
 
 #@dp.message_handler()
 async def echo_send(message : types.Message):
-    #await message.answer(message.text)
     await bot.send_message(message.from_user.id, 'Я ничего не понял. Пожалуйста, введите корректную команду. Список комманд можно узнать воспользовавшись коммандами /start или /help')
 
 
