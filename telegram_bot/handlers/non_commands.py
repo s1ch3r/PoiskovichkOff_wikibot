056222d1bb72627a16f405a2469ce5304411229b
s1ch3r <116456446+s1ch3r@users.noreply.github.com>
2022-12-19 02:54:01 +0300

    Update non_commands.py

--- telegram_bot/handlers/non_commands.py
+++ telegram_bot/handlers/non_commands.py
@@ -4,8 +4,19 @@ from create_bot import dp, bot
 
 #@dp.message_handler()
 async def echo_send(message : types.Message):
+    """
+    Функция сообщает о том что пользователь ввел некорректную команду
+    :param message:types.Message сообщение отправленное пользователем боту в телеграмм
+    :return: возвращает сообщение о вводе корректной команды и их список пользователю в телеграмм
+    """
     await bot.send_message(message.from_user.id, 'Я ничего не понял. Пожалуйста, введите корректную команду. Список комманд можно узнать воспользовавшись коммандами /start или /help')
 
 
 def register_handlers_non_commands(dp : Dispatcher):
+    """
+    Функция регистрирует ассинхронные фунуции библиотеки aiogram в этом файле
+    :param dp:Dispatcher диспетчер регистрирующий функции
+    :return:регистрирует ассинхронные фунуции библиотеки aiogram в этом файле для последующей передачи в __init__.py
+    а затем в bot_telegram.py
+    """
     dp.register_message_handler(echo_send)
