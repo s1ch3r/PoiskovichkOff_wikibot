a17b46a032b770273ec093fb6e4eff496b2aea57
s1ch3r <116456446+s1ch3r@users.noreply.github.com>
2022-12-18 23:31:59 +0300

    Update kb.py

--- telegram_bot/keyboards/kb.py
+++ telegram_bot/keyboards/kb.py
@@ -1,15 +1,20 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton#, ReplyKeyboardRemove
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
 b1 = KeyboardButton('/start')
 b2 = KeyboardButton('/Запрос')
 b3 = KeyboardButton('/Отмена')
 b4 = KeyboardButton('/Дополнительная_информация')
+b5 = KeyboardButton('/Случайная_статья')
+b6 = KeyboardButton('/Да')
+b7 = KeyboardButton('/Нет')
 
-kboard = ReplyKeyboardMarkup(resize_keyboard=True) #, one_time_keyboard=True
+kboard = ReplyKeyboardMarkup(resize_keyboard=True)
 cancel_kboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 additional_kboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+yesno_kboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 
-kboard.add(b1).add(b2)#.insert(b2)  .row(b1, b2)
+kboard.add(b1).add(b2).add(b5)#.insert(b2)  .row(b1, b2)
 cancel_kboard.add(b3)
-additional_kboard.row(b4, b3)
+additional_kboard.add(b4).add(b3)
+yesno_kboard.row(b6, b7)
