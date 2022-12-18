19930b1b14dab4985b2281c9fecbe1855c8bb755
s1ch3r <116456446+s1ch3r@users.noreply.github.com>
2022-12-19 02:54:35 +0300

    Update random.py

--- telegram_bot/handlers/random.py
+++ telegram_bot/handlers/random.py
@@ -19,6 +19,13 @@ class FSMRandom(StatesGroup):
 
 #@dp.message_handler(commands=['Random', 'random'])
 async def random_command(message : types.Message, state: FSMContext):
+    """
+    Функция находит случайную статью в российской википедии и спрашивает пользователя хочет ли он ее прочитать
+    :param message:types.Message сообщение отправленное пользователем боту в телеграмм
+    :param state:FSMContext состояние взятое из машины состояний, в котором находится эта функция
+    :return: возвращает название случайно полученной статьи из википедии и сообщение спрашивающее  пользователя
+    о намерении ее прочесть сообщением в телеграмм
+    """
     await FSMRandom.answer.set()
     await message.answer('Бим бим, бам бам, бом бом. Здесь скоро будет рандом!', reply_markup=ReplyKeyboardRemove())
     url = requests.get("https://ru.wikipedia.org/wiki/Special:Random")
@@ -26,12 +33,18 @@ async def random_command(message : types.Message, state: FSMContext):
     title = soup.find(class_="firstHeading").text
     async with state.proxy() as data:
         data['answer'] = title
-    await FSMRandom.next()
+    await FSMRandom.next()  # перевод на следующее состояние
     await message.answer(f"{title} \nВы хотите прочитать данную статью?", reply_markup=yesno_kboard)
 
 
 #выход из машины состояний
 async def cancel_random_command(message : types.Message, state: FSMContext):
+    """
+    Функция отменяет запрос пользователя о получении рандомной статьи из википедии
+    :param message:types.Message сообщение отправленное пользователем боту в телеграмм
+    :param state:FSMContext состояние взятое из машины состояний, в котором находится эта функция
+    :return: выходит из машины состояний и отправляет пользователю сообщение об отмене запроса в телеграмм
+    """
     current_state = await state.get_state()
     if current_state is None:
         return
@@ -41,21 +54,31 @@ async def cancel_random_command(message : types.Message, state: FSMContext):
 
 #@dp.message_handler(commands=['Да', 'Нет']) ###возможно нет надо отдельно
 async def confirmation_command(message : types.Message, state: FSMContext):
+    """
+    Функция отправляет пользователю статью из википедии, полученную в функции random_command
+    :param message:types.Message сообщение отправленное пользователем боту в телеграмм
+    :param state:FSMContext состояние взятое из машины состояний, в котором находится эта функция
+    :return: При подверждении от пользователя, отправляет пользователю в телеграмм 3 сообщения:
+    ссылку на полученную в random_command статью, ее название и содержание, а также выходит из машины состояний
+    """
     wikipedia.set_lang('ru')
     async with state.proxy() as data:
         question_wiki = wikipedia.page(str(data['answer']))
-    ans = message.text
-    if ans == "/Да" or ans == "Да" or ans == "да":
-        await message.answer('Ссылка на статью: \n' + question_wiki.url)
-        await message.answer('Название статьи: \n' + question_wiki.original_title)
-        await message.answer('Статья: \n' + question_wiki.summary + '\nЧтобы продолжить работу введите /start или /help')
-        await state.finish()
-    else:
-        await message.answer('Запрос отменен. Чтобы продолжить работу введите /start или /help')
-        await state.finish()
+    await message.answer('Ссылка на статью: \n' + question_wiki.url)
+    await message.answer('Название статьи: \n' + question_wiki.original_title)
+    await message.answer('Статья: \n' + question_wiki.summary + '\nЧтобы продолжить работу введите /start или /help')
+    await state.finish()
+
 
 
 def register_handlers_random(dp : Dispatcher):
+    """
+    Функция регистрирует ассинхронные фунуции библиотеки aiogram в этом файле и сверяет
+    сообщение пользователя со списком команд триггерящих их
+    :param dp:Dispatcher диспетчер регистрирующий функции
+    :return: регистрирует ассинхронные фунуции библиотеки aiogram в этом файле для последующей передачи в __init__.py
+    а затем в bot_telegram.py
+    """
     dp.register_message_handler(random_command, commands=['Случайная_статья', 'Рандомная_статья'])
     dp.register_message_handler(cancel_random_command, state="*", commands=['Нет'])
     dp.register_message_handler(cancel_random_command, Text(equals=['нет', 'Нет'], ignore_case=True), state="*")
