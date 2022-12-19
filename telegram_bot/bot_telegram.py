ca35587cab15e8af5e0a118969f7998a97cbf2eb
s1ch3r <116456446+s1ch3r@users.noreply.github.com>
2022-12-19 17:51:21 +0300

    Update bot_telegram.py

--- telegram_bot/bot_telegram.py
+++ telegram_bot/bot_telegram.py
@@ -2,21 +2,21 @@ from aiogram.utils import executor
 from create_bot import dp
 
 
-
-
 async def on_startup(_):
     """
     Функция оповещающая о начале работы телеграм бота
     :param _:
     :return: Возвращает в консоль сообщение об успешном запуске бота
     """
-    print('Бот успешно запущен. Скоро здесь будет служебная информация о боте. \nК примеру список его возможностей или источники из которых он берет информицию хз не придумал до конца ещё')
+    print(
+        'Бот успешно запущен. Скоро здесь будет служебная информация о боте. \nК примеру список его возможностей или источники из которых он берет информицию хз не придумал до конца ещё')
+
 
 from handlers import usual, wikipedia_request, random, non_commands
+
 usual.register_handlers_usual(dp)
 wikipedia_request.register_handlers_wikipedia_request(dp)
 random.register_handlers_random(dp)
 non_commands.register_handlers_non_commands(dp)
-
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
